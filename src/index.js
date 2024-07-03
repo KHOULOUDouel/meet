64e40ca447d65a4798315ba425bbec187d14b662
KHOULOUDouel <160501993+KHOULOUDouel@users.noreply.github.com>
2024-07-03 23:54:59 +0200

    Optimized Lambda function and updated Serverless configuration

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
+atatus.config('9dec0392dfb745e489c961f5caeb9071').install();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
